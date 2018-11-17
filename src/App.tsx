21f3e66731ecff765ac60fa2cd3f76d00f044864
Nicolas Schabram <n.schabram@cflox.com>
2018-11-17 11:34:58 +0100

    setup some global CSS rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React, { Component } from "react";
+import { injectGlobal } from 'emotion';
+import { normalize } from 'polished';
+import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return <div />;
+export class App extends Component {
+  public componentDidMount() {
+    injectGlobal`
+      ${normalize()}
+
+      * {
+        box-sizing: border-box;
+        position: relative;
+
+        user-select: none;
+
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        text-rendering: optimizeLegibility;
+
+        -webkit-tap-highlight-color: transparent;
+
+        &::before,
+        &::after {
+          box-sizing: inherit;
+        }
+      }
+    `;
+  }
+
+  public render() {
+    return <div>test</div>;
   }
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // XXX: Enable service worker.
 // If you want your app to work offline and load faster, you can change

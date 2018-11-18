469c3c2ed3dd899f819484f2d345aa12229768cd
Nicolas Schabram <n.schabram@cflox.com>
2018-11-18 03:23:51 +0100

    move winner term to the center of the screen and increase size

--- src/components/LetterWall.tsx
+++ src/components/LetterWall.tsx
@@ -1,7 +1,7 @@
 import Rect from "@reach/rect";
 import WindowSize from "@reach/window-size";
 import { css, cx } from "emotion";
-import React, { memo, SFC } from "react";
+import React, { Component, memo } from "react";
 import { CharController, TermDefMap } from "./CharController";
 
 type Props = {
@@ -16,169 +16,229 @@ type Props = {
   ) => JSX.Element;
 };
 
-const COMPUTATION_DURATION = "3.75s";
+type State = { isResultMode: boolean };
 
-const LetterWall: SFC<Props> = ({ numberOfLetters, children }) => (
-  <CharController numberOfLetters={numberOfLetters}>
-    {({ registerTerm, chars, terms, pickWinner, winner }) => (
-      <WindowSize>
-        {(size: any) => (
-          <div
-            className={css`
-              display: flex;
-              flex-direction: column;
-              height: 100%;
-              width: 100%;
-              width: ${size.width}px;
-              height: ${size.height}px;
-            `}
-          >
-            <div
-              className={css`
-                z-index: 500;
-              `}
-            >
-              {children({ registerTerm, pickWinner, winner, terms })}
-            </div>
-            <div
-              className={css`
-                flex-grow: 1;
-                padding: 1rem;
-                max-width: 100%;
-                display: flex;
-                flex-direction: column;
-                align-items: stretch;
-              `}
-            >
-              <Rect>
-                {({ ref, rect }: any) => (
-                  <ul
-                    ref={ref}
-                    className={css`
-                      flex-grow: 1;
-                      width: 100%;
-                      height: 100%;
-                      display: flex;
-                      flex-wrap: wrap;
-                      justify-content: space-between;
-                      list-style-type: none;
-                      margin: 0;
-                      padding: 0;
+const COMPUTATION_DURATION = 3.75;
 
-                      perspective: 1000px;
-                    `}
-                  >
-                    {(() => {
-                      if (!rect) {
-                        return null;
-                      }
+class LetterWall extends Component<Props> {
+  public state: State = { isResultMode: false };
 
-                      const squareLength = `${Math.sqrt(
-                        (rect.width * rect.height) / numberOfLetters
-                      )}px`;
+  public render() {
+    const {
+      props: { numberOfLetters, children },
+      state: { isResultMode }
+    } = this;
 
-                      return Object.keys(chars)
-                        .sort()
-                        .map(position => {
-                          return (
-                            <li
-                              key={chars[position].position}
-                              // For flip animation, see https://davidwalsh.name/css-flip.
-                              className={cx(
-                                css`
-                                  text-align: center;
-                                  text-transform: uppercase;
-                                  font-size: calc(${squareLength} * 0.55);
-                                  font-weight: 400;
-                                  transition: 0.5s ease-out;
-                                  transform-style: preserve-3d;
-                                  position: relative;
-                                  margin: 0.15rem;
-                                  width: calc(${squareLength} - 0.3rem);
-                                  line-height: calc(${squareLength} - 0.3rem);
-                                  background-color: #f5f5f5;
-                                  box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.15);
-                                  border-radius: 2px;
+    return (
+      <CharController numberOfLetters={numberOfLetters}>
+        {({ registerTerm, chars, terms, pickWinner, winner }) => (
+          <WindowSize>
+            {(size: any) => (
+              <div
+                className={css`
+                  display: flex;
+                  flex-direction: column;
+                  height: 100%;
+                  width: 100%;
+                  width: ${size.width}px;
+                  height: ${size.height}px;
+                `}
+              >
+                <div
+                  className={css`
+                    z-index: 500;
+                  `}
+                >
+                  {children({
+                    registerTerm,
+                    pickWinner: () => {
+                      setTimeout(
+                        () => this.setState({ isResultMode: true }),
+                        (COMPUTATION_DURATION + 1.75) * 1000
+                      );
+                      pickWinner();
+                    },
+                    winner,
+                    terms
+                  })}
+                </div>
+                <div
+                  className={css`
+                    flex-grow: 1;
+                    padding: 1rem;
+                    max-width: 100%;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: stretch;
+                  `}
+                >
+                  <Rect>
+                    {({ ref, rect }: any) => (
+                      <ul
+                        ref={ref}
+                        className={css`
+                          flex-grow: 1;
+                          width: 100%;
+                          height: 100%;
+                          display: flex;
+                          flex-wrap: wrap;
+                          justify-content: ${isResultMode
+                            ? "center"
+                            : "space-between"};
+                          align-items: center;
+                          align-content: center;
+                          list-style-type: none;
+                          margin: 0;
+                          padding: 0;
+                          perspective: 1000px;
+                        `}
+                      >
+                        {(() => {
+                          if (!rect) {
+                            return null;
+                          }
 
-                                  > span {
-                                    display: block;
-                                    width: 100%;
-                                    height: 100%;
-                                    border-radius: 2px;
-                                  }
+                          const squareLength = `${Math.sqrt(
+                            (rect.width * rect.height) / numberOfLetters
+                          )}px`;
 
-                                  > span {
-                                    backface-visibility: hidden;
-                                    position: absolute;
-                                    top: 0;
-                                    left: 0;
-                                  }
-                                `,
-                                chars[position].fixedChar &&
-                                  css`
-                                    transform: rotateY(180deg);
-                                  `,
-                                winner &&
-                                  css`
-                                    transition-duration: ${COMPUTATION_DURATION};
-                                    transform: rotateY(
-                                      ${chars[position].fixedChar &&
-                                      chars[position].term !== winner
-                                        ? `${[4, 5, 6, 7, 8, 9][
-                                            Math.floor(Math.random() * 5)
-                                          ] * 360}deg`
-                                        : `${[4, 5, 6, 7, 8, 9][
-                                            Math.floor(Math.random() * 5)
-                                          ] *
-                                            360 +
-                                            180}deg`}
-                                    );
-                                  `
-                              )}
-                            >
-                              {/* XXX: Fill up last row. */}
-                              <span
-                                className={cx(
-                                  css`
-                                    z-index: 2;
-                                    transform: rotateY(0deg);
-                                  `
-                                )}
-                              >
-                                {chars[position].fixedChar ||
-                                  chars[position].randomChar}
-                              </span>
-                              <span
-                                className={cx(
-                                  css`
-                                    transform: rotateY(180deg);
-                                    color: black;
-                                  `,
-                                  chars[position].fixedChar &&
+                          return Object.keys(chars)
+                            .sort()
+                            .map(position => {
+                              return (
+                                <li
+                                  key={chars[position].position}
+                                  // For flip animation, see https://davidwalsh.name/css-flip.
+                                  className={cx(
                                     css`
-                                      color: white;
-                                      background-color: ${terms[
-                                        chars[position].term!
-                                      ].color};
-                                    `
-                                )}
-                              >
-                                {chars[position].fixedChar ||
-                                  chars[position].randomChar}
-                              </span>
-                            </li>
-                          );
-                        });
-                    })()}
-                  </ul>
-                )}
-              </Rect>
-            </div>
-          </div>
+                                      text-align: center;
+                                      text-transform: uppercase;
+                                      font-size: calc(${squareLength} * 0.55);
+                                      font-weight: 400;
+                                      transition: 0.5s ease-out;
+                                      transform-style: preserve-3d;
+                                      position: relative;
+                                      margin: 0.15rem;
+                                      width: calc(${squareLength} - 0.3rem);
+
+                                      height: calc(${squareLength} - 0.3rem);
+                                      display: block;
+                                      background-color: #f5f5f5;
+                                      box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.15);
+                                      border-radius: 2px;
+
+                                      > span {
+                                        display: block;
+                                        width: 100%;
+                                        height: 100%;
+                                        border-radius: 2px;
+                                        line-height: 1;
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                      }
+                                      > span {
+                                        backface-visibility: hidden;
+                                        position: absolute;
+                                        top: 0;
+                                        left: 0;
+                                      }
+                                    `,
+                                    chars[position].fixedChar &&
+                                      css`
+                                        transform: rotateY(180deg);
+                                      `,
+                                    winner &&
+                                      !isResultMode &&
+                                      css`
+                                        transition-duration: ${COMPUTATION_DURATION}s;
+                                        transform: rotateY(
+                                          ${chars[position].fixedChar &&
+                                          chars[position].term !== winner
+                                            ? `${[4, 5, 6, 7, 8, 9][
+                                                Math.floor(Math.random() * 5)
+                                              ] * 360}deg`
+                                            : `${[4, 5, 6, 7, 8, 9][
+                                                Math.floor(Math.random() * 5)
+                                              ] *
+                                                360 +
+                                                180}deg`}
+                                        );
+                                      `,
+                                    isResultMode &&
+                                      chars[position].term !== winner &&
+                                      css`
+                                        transition: all 1s ease-out;
+                                        flex-basis: 0;
+                                        width: 0;
+                                        margin: 0;
+                                        border: 0;
+                                        padding: 0;
+                                        opacity: 0;
+                                        overflow: hidden;
+                                      `,
+
+                                    isResultMode &&
+                                      chars[position].term === winner &&
+                                      css`
+                                        transition: all 1s ease-out;
+                                        width: calc(
+                                          2 * ${squareLength} - 0.3rem
+                                        );
+                                        height: calc(
+                                          2 * ${squareLength} - 0.3rem
+                                        );
+                                        font-size: calc(
+                                          2 * ${squareLength} * 0.55
+                                        );
+                                      `
+                                  )}
+                                >
+                                  {/* XXX: Fill up last row. */}
+                                  <span
+                                    className={cx(
+                                      css`
+                                        z-index: 2;
+                                        transform: rotateY(0deg);
+                                      `
+                                    )}
+                                  >
+                                    {chars[position].fixedChar ||
+                                      chars[position].randomChar}
+                                  </span>
+                                  <span
+                                    className={cx(
+                                      css`
+                                        transform: rotateY(180deg);
+                                        color: black;
+                                      `,
+                                      chars[position].fixedChar &&
+                                        css`
+                                          color: white;
+                                          background-color: ${chars[position]
+                                            .fixedChar !== " " &&
+                                            terms[chars[position].term!].color};
+                                        `
+                                    )}
+                                  >
+                                    {chars[position].fixedChar ||
+                                      chars[position].randomChar}
+                                  </span>
+                                </li>
+                              );
+                            });
+                        })()}
+                      </ul>
+                    )}
+                  </Rect>
+                </div>
+              </div>
+            )}
+          </WindowSize>
         )}
-      </WindowSize>
-    )}
-  </CharController>
-);
+      </CharController>
+    );
+  }
+}
 
 export default memo(LetterWall);

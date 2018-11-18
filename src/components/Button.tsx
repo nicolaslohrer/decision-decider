2b084981a31ecc4e4389f11c40bc0adb31f96e08
Nicolas Schabram <n.schabram@cflox.com>
2018-11-18 01:55:24 +0100

    tweak button styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ const colorCycle = () => keyframes`
     let keyframes: string = "";
     shuffleArray(COLORS).forEach((color, i) => {
       keyframes += `${(100 / (COLORS.length - 1)) *
-        i}% { border-color: ${color}; background-color: ${color}; }`;
+        i}% { background-color: ${color}; }`;
     });
     return keyframes;
   })()}
@@ -17,11 +17,16 @@ export const Button = styled("button")`
   border-radius: 3px;
   width: 100%;
   line-height: 2;
-  border: 2px solid grey;
+  border: 0 none;
   font-weight: 700;
   font-size: 1rem;
   color: white;
   background: none;
   letter-spacing: 0.25ch;
   animation: ${colorCycle()} ${COLORS.length * 5}s infinite;
+  transition: opacity 0.35s ease-out;
+
+  &:hover {
+    opacity: 0.85;
+  }
 `;

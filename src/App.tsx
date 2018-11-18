c161186d3aa55fc54264fae837f425466fb4cb4a
Nicolas Schabram <n.schabram@cflox.com>
2018-11-18 04:19:25 +0100

    add todos

--- src/App.tsx
+++ src/App.tsx
@@ -55,6 +55,8 @@ export class App extends Component<{}, State> {
           )}
         </LetterWall>
       </ErrorBoundary>
+      // XXX: Add imprint.
+      // XXX: Fix spacing issues.
     );
   }
 }

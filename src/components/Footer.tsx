776c30d2757ba079e9971f2704b934376a287f8b
Nicolas Schabram <n.schabram@cflox.com>
2018-12-15 16:44:07 +0100

    migrate footer to hooks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 import "@reach/dialog/styles.css";
-import { Component } from "react";
+import { FunctionComponent, useState } from "react";
 import { Dialog } from "./Dialog";
 import { Imprint } from "./Imprint";
 import { Privacy } from "./Privacy";
@@ -9,99 +9,86 @@ import { Privacy } from "./Privacy";
 jsx;
 
 type Props = { className?: string };
-type State = { isImprintVisible: boolean; isPrivacyVisible: boolean };
 
-export class Footer extends Component<Props, State> {
-  public state: State = { isImprintVisible: false, isPrivacyVisible: false };
+export const Footer: FunctionComponent<Props> = ({ className }) => {
+  const [isImprintVisible, setIsImprintVisible] = useState(false);
+  const [isPrivacyVisible, setIsPrivacyVisible] = useState(false);
 
-  public render() {
-    const {
-      props: { className },
-      state: { isPrivacyVisible, isImprintVisible }
-    } = this;
+  return (
+    <footer
+      css={[
+        css`
+          padding: 1.25vh 2vh;
+          border-top: 1px solid black;
+          font-size: 0.6rem;
+          opacity: 0.6;
+          transition: opacity 0.25s ease-out;
 
-    return (
-      <footer
-        css={[
-          css`
-            padding: 1.25vh 2vh;
-            border-top: 1px solid black;
-            font-size: 0.6rem;
-            opacity: 0.6;
-            transition: opacity 0.25s ease-out;
+          &:hover,
+          &:hover {
+            opacity: 1;
+          }
+        `
+      ]}
+      className={className}
+    >
+      <ul
+        css={css`
+          display: flex;
+          list-style-type: none;
+          margin: 0;
+          padding: 0;
+          justify-content: center;
 
-            &:hover,
-            &:hover {
-              opacity: 1;
-            }
-          `
-        ]}
-        className={className}
-      >
-        <ul
-          css={css`
+          li {
             display: flex;
-            list-style-type: none;
-            margin: 0;
-            padding: 0;
-            justify-content: center;
 
-            li {
-              display: flex;
-
-              &::before {
-                content: "※";
-                margin-right: 1ch;
-              }
-
-              + li {
-                display: inline-block;
-                margin-left: 2.5ch;
-              }
+            &::before {
+              content: "※";
+              margin-right: 1ch;
             }
-          `}
-        >
-          <li>
-            <span>
-              A weird side project by{" "}
-              <a href="http://nicolasschabram.com">Nicolas Schabram</a>
-            </span>
-          </li>
-          <li>
-            <span>
-              <a
-                href="#0"
-                onClick={() => this.setState({ isImprintVisible: true })}
-              >
-                Imprint
-              </a>
-            </span>
-          </li>
-          <li>
-            <span>
-              <a
-                href="#0"
-                onClick={() => this.setState({ isPrivacyVisible: true })}
-              >
-                Privacy
-              </a>
-            </span>
-          </li>
-        </ul>
 
-        <Dialog
-          isOpen={isImprintVisible}
-          onDismiss={() => this.setState({ isImprintVisible: false })}
-        >
-          <Imprint />
-        </Dialog>
-        <Dialog
-          isOpen={isPrivacyVisible}
-          onDismiss={() => this.setState({ isPrivacyVisible: false })}
-        >
-          <Privacy />
-        </Dialog>
-      </footer>
-    );
-  }
-}
+            + li {
+              display: inline-block;
+              margin-left: 2.5ch;
+            }
+          }
+        `}
+      >
+        <li>
+          <span>
+            A weird side project by{" "}
+            <a href="http://nicolasschabram.com">Nicolas Schabram</a>
+          </span>
+        </li>
+        <li>
+          <span>
+            <a href="#0" onClick={() => setIsImprintVisible(true)}>
+              Imprint
+            </a>
+          </span>
+        </li>
+        <li>
+          <span>
+            <a href="#0" onClick={() => setIsPrivacyVisible(true)}>
+              Privacy
+            </a>
+          </span>
+        </li>
+      </ul>
+
+      <Dialog
+        isOpen={isImprintVisible}
+        onDismiss={() => setIsImprintVisible(false)}
+      >
+        <Imprint />
+      </Dialog>
+      <Dialog
+        isOpen={isPrivacyVisible}
+        onDismiss={() => setIsPrivacyVisible(false)}
+      >
+        <Privacy />
+      </Dialog>
+    </footer>
+  );
+};

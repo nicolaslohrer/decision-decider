0d5b32d02a2637fd9f5a3cc3646ef700baaf10a7
Nicolas Schabram <n.schabram@cflox.com>
2019-02-03 11:17:51 +0100

    get rid of unnecessary div wrapping CharWall

--- src/decider/CharWall.tsx
+++ src/decider/CharWall.tsx
@@ -42,148 +42,137 @@ export const CharWall: FunctionComponent = memo(() => {
         }
 
         return (
-          <div
+          <ul
+            ref={ref}
             css={css`
               flex-grow: 1;
+              /* XXX: Try https://css-tricks.com/auto-sizing-columns-css-grid-auto-fill-vs-auto-fit/ */
               display: flex;
-              flex-direction: column;
-              align-items: stretch;
+              flex-wrap: wrap;
+              justify-content: ${["FILTERING_LETTERS", "DONE"].includes(
+                lifecyclePhase
+              )
+                ? "center"
+                : "space-between"};
+              align-items: center;
+              align-content: center;
+              list-style-type: none;
+              margin: 0;
+              padding: 0;
+              perspective: 1000px;
             `}
           >
-            <ul
-              ref={ref}
-              css={css`
-                flex-grow: 1;
-                height: 100%;
-                display: flex;
-                flex-wrap: wrap;
-                justify-content: ${["FILTERING_LETTERS", "DONE"].includes(
-                  lifecyclePhase
-                )
-                  ? "center"
-                  : "space-between"};
-                align-items: center;
-                align-content: center;
-                list-style-type: none;
-                margin: 0;
-                padding: 0;
-                perspective: 1000px;
-              `}
-            >
-              {Object.keys(charsWithDummies)
-                .map(k => Number(k))
-                .sort((a, b) => a - b)
-                .map(position => (
-                  <li
-                    // TODO: Fix card animations in Safari on iOS.
-                    key={charsWithDummies[position].position}
-                    // For flip animation, see https://davidwalsh.name/css-flip.
+            {Object.keys(charsWithDummies)
+              .map(k => Number(k))
+              .sort((a, b) => a - b)
+              .map(position => (
+                <li
+                  // TODO: Fix card animations in Safari on iOS.
+                  key={charsWithDummies[position].position}
+                  // For flip animation, see https://davidwalsh.name/css-flip.
+                  css={[
+                    css`
+                      text-align: center;
+                      text-transform: uppercase;
+                      font-size: calc(${squareSize}px * 0.55);
+                      font-weight: 400;
+                      transition: 0.5s ease-out;
+                      transform-style: preserve-3d;
+                      position: relative;
+                      width: ${squareSize}px;
+                      height: ${squareSize}px;
+                      display: block;
+                      border-radius: 2px;
+                      background-color: #f5f5f5;
+                      border: ${squareSize * 0.03}px solid white;
+
+                      > span {
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        border-radius: 2px;
+                        line-height: 1;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        backface-visibility: hidden;
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                      }
+                    `,
+                    charsWithDummies[position].fixedChar &&
+                      css`
+                        transform: rotateY(180deg);
+                      `,
+                    ["ROTATING_LETTERS", "FILTERING_LETTERS", "DONE"].includes(
+                      lifecyclePhase
+                    ) &&
+                      css`
+                        transition-duration: ${LETTER_ROTATION_DURATION}ms;
+                        transform: rotateY(
+                          ${charsWithDummies[position].term !== winner
+                            ? `${[4, 5, 6, 7, 8, 9][
+                                Math.floor(Math.random() * 5)
+                              ] * 360}deg`
+                            : `${[4, 5, 6, 7, 8, 9][
+                                Math.floor(Math.random() * 5)
+                              ] *
+                                360 +
+                                180}deg`}
+                        );
+                      `,
+                    ["FILTERING_LETTERS", "DONE"].includes(lifecyclePhase) &&
+                      (charsWithDummies[position].term === winner
+                        ? css`
+                            transition: all ${LETTER_FILTERING_DURATION}ms
+                              ease-out;
+                            width: calc(1.5 * ${squareSize}px);
+                            height: calc(1.5 * ${squareSize}px);
+                            font-size: calc(1 * ${squareSize}px);
+                          `
+                        : css`
+                            transition: all ${LETTER_FILTERING_DURATION}ms
+                              ease-out;
+                            transform: scale(0.5);
+                            width: 0;
+                            margin: 0;
+                            border: 0;
+                            padding: 0;
+                            opacity: 0;
+                            overflow: hidden;
+                          `)
+                  ]}
+                >
+                  <span
                     css={[
                       css`
-                        text-align: center;
-                        text-transform: uppercase;
-                        font-size: calc(${squareSize}px * 0.55);
-                        font-weight: 400;
-                        transition: 0.5s ease-out;
-                        transform-style: preserve-3d;
-                        position: relative;
-                        width: ${squareSize}px;
-                        height: ${squareSize}px;
-                        display: block;
-                        border-radius: 2px;
-                        background-color: #f5f5f5;
-                        border: ${squareSize * 0.03}px solid white;
-
-                        > span {
-                          display: block;
-                          width: 100%;
-                          height: 100%;
-                          border-radius: 2px;
-                          line-height: 1;
-                          display: flex;
-                          justify-content: center;
-                          align-items: center;
-                          backface-visibility: hidden;
-                          position: absolute;
-                          top: 0;
-                          left: 0;
-                        }
+                        z-index: 2;
+                        transform: rotateY(0deg);
+                      `
+                    ]}
+                  />
+                  <span
+                    css={[
+                      css`
+                        transform: rotateY(180deg);
+                        color: black;
                       `,
                       charsWithDummies[position].fixedChar &&
                         css`
-                          transform: rotateY(180deg);
-                        `,
-                      [
-                        "ROTATING_LETTERS",
-                        "FILTERING_LETTERS",
-                        "DONE"
-                      ].includes(lifecyclePhase) &&
-                        css`
-                          transition-duration: ${LETTER_ROTATION_DURATION}ms;
-                          transform: rotateY(
-                            ${charsWithDummies[position].term !== winner
-                              ? `${[4, 5, 6, 7, 8, 9][
-                                  Math.floor(Math.random() * 5)
-                                ] * 360}deg`
-                              : `${[4, 5, 6, 7, 8, 9][
-                                  Math.floor(Math.random() * 5)
-                                ] *
-                                  360 +
-                                  180}deg`}
-                          );
-                        `,
-                      ["FILTERING_LETTERS", "DONE"].includes(lifecyclePhase) &&
-                        (charsWithDummies[position].term === winner
-                          ? css`
-                              transition: all ${LETTER_FILTERING_DURATION}ms
-                                ease-out;
-                              width: calc(1.5 * ${squareSize}px);
-                              height: calc(1.5 * ${squareSize}px);
-                              font-size: calc(1 * ${squareSize}px);
-                            `
-                          : css`
-                              transition: all ${LETTER_FILTERING_DURATION}ms
-                                ease-out;
-                              transform: scale(0.5);
-                              width: 0;
-                              margin: 0;
-                              border: 0;
-                              padding: 0;
-                              opacity: 0;
-                              overflow: hidden;
-                            `)
+                          color: white;
+                          background-color: ${charsWithDummies[position]
+                            .fixedChar !== " " &&
+                            terms[charsWithDummies[position].term!].color};
+                        `
                     ]}
                   >
-                    <span
-                      css={[
-                        css`
-                          z-index: 2;
-                          transform: rotateY(0deg);
-                        `
-                      ]}
-                    />
-                    <span
-                      css={[
-                        css`
-                          transform: rotateY(180deg);
-                          color: black;
-                        `,
-                        charsWithDummies[position].fixedChar &&
-                          css`
-                            color: white;
-                            background-color: ${charsWithDummies[position]
-                              .fixedChar !== " " &&
-                              terms[charsWithDummies[position].term!].color};
-                          `
-                      ]}
-                    >
-                      {charsWithDummies[position].fixedChar ||
-                        charsWithDummies[position].randomChar}
-                    </span>
-                  </li>
-                ))}
-            </ul>
-          </div>
+                    {charsWithDummies[position].fixedChar ||
+                      charsWithDummies[position].randomChar}
+                  </span>
+                </li>
+              ))}
+          </ul>
         );
       }}
     </Rect>

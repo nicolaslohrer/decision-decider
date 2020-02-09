a2d52c6314a467471d1ab145aa04e77bf6d6680d
Nicolas Schabram <n.schabram@cflox.com>
2020-02-09 17:34:21 +0100

    update reach helper libs; refs #6

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,5 @@
 // XXX: Can we get rid of some these after updating the packages?
-declare module "@reach/rect";
 declare module "@reach/window-size";
 declare module "@reach/dialog";
-declare module "@reach/visually-hidden";
 declare module "@reach/menu-button";
 declare module "react-remarkable";

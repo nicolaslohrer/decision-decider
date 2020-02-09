2a011f7ccb2c5856e9e652f05fa819caea6930e8
Nicolas Schabram <n.schabram@cflox.com>
2020-02-09 17:45:39 +0100

    update @reach/menu-button; refs #6

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,5 +1,2 @@
 // XXX: Can we get rid of some these after updating the packages?
-declare module "@reach/window-size";
-declare module "@reach/dialog";
-declare module "@reach/menu-button";
 declare module "react-remarkable";
